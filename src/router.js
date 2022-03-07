cdd43c09decf6bfb45a3816d23e22df61d5d5057
Vishwas <vishwasanand.cse@gmail.com>
2022-03-07 08:59:18 +0530

    added one more path to fix nginx issue

--- src/router.js
+++ src/router.js
@@ -14,6 +14,10 @@ const router =  new Router({
   mode: 'history',
   routes: [
     {
+      path: '/explorer',
+      redirect: '/explorer/dashboard'
+    },
+    {
       path: '/',
       redirect: '/explorer/dashboard'
     },
