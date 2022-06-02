0a090212ca54b5c140962d623a0675c749ac7df7
Vishwas <vishwasanand.cse@gmail.com>
2022-06-02 19:29:06 +0530

    udpated

--- src/router.js
+++ src/router.js
@@ -65,14 +65,14 @@ const router = new Router({
     ]
 })
 
-router.beforeEach((to, from, next) => {
-    // Redirect if fullPath begins with a hash (ignore hashes later in path)
-    if (to.fullPath.substr(0, 2) === "/#") {
-        const path = to.fullPath.substr(2);
-        next(path);
-        return;
-    }
-    next();
-});
+// router.beforeEach((to, from, next) => {
+//     // Redirect if fullPath begins with a hash (ignore hashes later in path)
+//     if (to.fullPath.substr(0, 2) === "/#") {
+//         const path = to.fullPath.substr(2);
+//         next(path);
+//         return;
+//     }
+//     next();
+// });
 
 export default router
